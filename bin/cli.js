e1107bc00291c1a456688dff4333072d738c8005
何遇 <1395294694@qq.com>
2020-01-20 13:57:23 +0800

    feat: set remote preset

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 const vueCliBinPath = require.resolve('@vue/cli/bin/vue');
-let args = process.argv.slice(2);
+const args = process.argv.slice(2);
 
 if(args[0] === 'create' && !args.includes('--preset')) {
   args.push('--preset');
-  args.push('yuezhilunhui2009/vue-cli3-preset-seed')
+  args.push('QxQstar/dmallmax-vue-preset')
 }
 
 require('child_process').fork(
