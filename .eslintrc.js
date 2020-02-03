aee21f5025b39d044ed0085b6b82c9a9ffe65a54
何遇 <1395294694@qq.com>
2020-02-03 15:53:27 +0800

    chore: 修改 eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,18 @@
 module.exports = {
-  extends: [
-    "plugin:vue-libs/recommended"
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended"
   ],
-  plugins: [
-    "node"
-  ],
-  env: {
-    "jest": true
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "sourceType": "module"
   },
-  globals: {
-    name: 'off'
-  },
-  rules: {
-    "indent": ["error", 2, {
-      "MemberExpression": "off"
-    }],
-    "node/no-extraneous-require": ["error", {
-      "allowModules": [
-        "@vue/cli-test-utils"
-      ]
-    }]
-  },
-  overrides: [
-    {
-      files: ['**/__tests__/**/*.js', "**/cli-test-utils/**/*.js"],
-      rules: {
-        "node/no-extraneous-require": "off"
-      }
-    }
-  ]
-}
+  "rules": {
+    "no-prototype-builtins":["off"],
+    "no-useless-catch":["off"]
+  }
+};
--- bin/cli.js
+++ bin/cli.js
@@ -4,7 +4,8 @@ const args = process.argv.slice(2);
 
 if(args[0] === 'create' && !args.includes('--preset')) {
   args.push('--preset');
-  args.push('QxQstar/dmallmax-vue-preset')
+  // args.push('QxQstar/dmallmax-vue-preset')
+  args.push('../dmallmax-vue-preset')
 }
 
 require('child_process').fork(
