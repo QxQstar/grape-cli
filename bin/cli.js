e5db4b56c08d02801855d425a23ddb3c7503cbc1
何遇 <1395294694@qq.com>
2020-02-19 15:24:11 +0800

    feat: 修改preset的路径

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 const vueCliBinPath = require.resolve('@vue/cli/bin/vue');
 const args = process.argv.slice(2);
+const path = require('path')
 
 if(args[0] === 'create' && !args.includes('--preset') && !args.includes('-p')) {
   args.push('--preset');
-  args.push('./preset')
+  args.push(path.resolve(__dirname,'../preset'))
 
   if(!args.includes('--bare') && !args.includes('-b')) {
     args.push('--bare')
